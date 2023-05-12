d1d42431ac93d059c585da3c90d38090a0e51b4e
blackSean <idowuoriloye@gmail.com>
2023-05-12 14:21:37 +0100

    hardhat config updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
+
+type HttpNetworkAccountsUserConfig = any;
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.10",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+      chainId: 44787,
+    }
+  }
 };
 
 export default config;
